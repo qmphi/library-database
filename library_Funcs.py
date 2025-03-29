e3eb24a8835a40a370796c611b7e336865f512da
sca383 <sca383@sfu.ca>
2025-03-28 23:10:17 -0700

    updates

---
+++ library_Funcs.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+database = 'library.db'
+
+#reminder need to open and close db connection in each 
+
+def browseItems()
+    
+
+#add ID systematically
+def addUser(firstName, lastName, phoneNum)
+    
+def borrowItem(itemID, userID)
+    
+def returnItem(itemID, userID)
+    
+
--- main.py
+++ main.py
@@ -3,13 +3,12 @@ def main_menu():
     menu = """\nWelcome to the library!
 To navigate, please use the following instructions:
 
-  1 - Browse items
-  2 - To borrow an item
-  3 - To return an item
-  4 - View library events
-  5 - Donate an item
-  6 - Apply for volunteering
-  7 - Help
+  1 - Browse 
+  2 - To return an item
+  3 - View library events
+  4 - Donate an item
+  5 - Apply for volunteering
+  6 - Help
   0 - Exit the program
     """
     print(menu)
@@ -17,27 +16,25 @@ To navigate, please use the following instructions:
 userInput = -1
 
 while (userInput != 0):
-    ##if not logged in, can't borrow, return, register for events##
 
     main_menu()
     userInput = int(input("I want to navigate to: "))
     if (userInput == 1): #Browse items
-        #--> can search by different filters
-        #searchFunc
+        #--> can search by different filters (id, genre, type etc)
+            #-->prompt user if they want to borrow, ask for userID. 
+                #--> If no userID, prompt new user?
         continue
-    elif (userInput == 2): #borrow an item
+    elif (userInput == 2): #Manage loans (return and item info/fines)
         continue
-    elif (userInput == 3): #return an item
-        continue
-    elif (userInput == 4): #View library events
+    elif (userInput == 3): #View library events
         #View events --> can filter by audience, type, time, etc
         # register for event
         continue
-    elif (userInput == 5): #Donate an item
+    elif (userInput == 4): #Donate an item
         continue
-    elif (userInput == 6): # Apply for volunteering
+    elif (userInput == 5): # Apply for volunteering
         continue
-    elif (userInput == 7): #Help
+    elif (userInput == 6): #Help
         continue
     elif (userInput == 0): #Exit program
         break
