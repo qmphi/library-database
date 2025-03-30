efeddcd9978a36a2227c719543c3d8c15d5238ff
sca383 <sca383@sfu.ca>
2025-03-29 22:52:56 -0700

    backDB script

---
+++ backupDBScript.py
@@ -0,0 +1,16 @@
+import sqlite3
+import shutil
+
+database = 'libraryClean.db'
+backup_file = 'libraryCleanBackup.db'
+
+def backup_database():
+    """Creates a backup copy of the database."""
+    try:
+        shutil.copy(database, backup_file)
+        print("\nDatabase backup created successfully!")
+    except Exception as e:
+        print("Error during backup:", e)
+
+if __name__ == "__main__":
+    backup_database()
